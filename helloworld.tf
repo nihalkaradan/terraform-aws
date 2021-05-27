00168a845e7604ec84b9ba89418a340d562b3659
Nihal Karadan <nihalkaradan@gmail.com>
2021-05-27 09:16:02 +0530

    strings map lists

--- helloworld.tf
+++ helloworld.tf
@@ -1,3 +1,33 @@
 provider "aws"{
   profile = "nihal-terraform"
+  region = "ap-south-1"
+}
+//string example
+variable "firststring"{
+  type = string
+  default = "my first string"
+}
+output "firstoutput" {
+  value = "${var.firststring}"
+}
+//map example
+variable "mapexample" {
+  type = map
+  default = {
+    "useast" = "ami1"
+    "uswest" = "ami2"
+  }
+}
+output "mapoutput" {
+  value = "${var.mapexample["useast"]}"
+}
+//List example
+variable "sg" {
+  type = list
+  default = [
+    "sg1","sg2"
+  ]
+}
+output "sgoutput" {
+  value = "${var.sg[0]}"
 }
